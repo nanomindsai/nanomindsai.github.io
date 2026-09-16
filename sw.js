93646776893ffd0e3d768353fa19ba246d2c61cc
Omer Erdogan <oerdogan@example.com>
2026-09-17 01:33:40 +0300

    Nanominds landing page

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install',e=>{self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil((async()=>{
+  const ks=await caches.keys();await Promise.all(ks.map(k=>caches.delete(k)));
+  await self.registration.unregister();
+  const cs=await self.clients.matchAll();cs.forEach(c=>c.navigate(c.url));
+})())});
